98933a5eea96283344df3efb1760f2512c7728cb
Rawat Pallavi <rpallavi@thoughtworks.com>
2019-12-18 23:54:07 +0530

    add fbapi and fblogin all into window

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function login() {
+    window.fblogin()
+    console.log("inside the login of react")
+  }
+
+  function userData() {
+    window.fbApi()
+    console.log("inside the eserdata of react")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <input type={"button"} onClick={login} value={"login with fb"}/>
+        <input type={"button"} onClick={userData} value={"print fb data"}/>
       </header>
     </div>
   );
